88f3086cca4fc0b9a98b4dc4bb4157d4b1d8b5ef
FrancescoXD <28204054+FrancescoXD@users.noreply.github.com>
2021-08-06 01:04:27 +0200

    update

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # easyfetch
-A very simple rust linux fetcher
+A very simple and bad coded rust linux fetcher. Works only on ArchLinux.
--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn main() {
     let shell = shell.split("/").last().unwrap();
 
     // Kernel
-    let kernel = fs::read_to_string("/proc/sys/kernel/osrelease").unwrap();
+    let kernel = fs::read_to_string("/proc/sys/kernel/osrelease").unwrap().split_whitespace().next().unwrap().to_owned();
 
     // Uptime
     let uptime = fs::read_to_string("/proc/uptime").unwrap();
@@ -25,7 +25,12 @@ fn main() {
         Err(_) => 0.00,
     };
 
-    let uptime = (uptime / 60.00) as i32;
+    let mut hours = 0;
+    let mut minutes = (uptime / 60.00) as i32;
+    if minutes > 60 {
+        hours = minutes / 60;
+        minutes -= hours * 60;
+    }
 
     // Memory
     let memory = fs::read_to_string("/proc/meminfo").unwrap();
@@ -40,16 +45,28 @@ fn main() {
     let totmemused = memtotal - memfree;
     let memused = totmemused - (buffers + cached);
 
-    // Packages - needs to be fixed
+    // Packages - Works only with arch
     let output = Command::new("/bin/bash")
         .args(&["-c", "pacman -Q | wc -l"])
         .output()
         .expect("Failed to execute pacman");
-    //println!("{:?}", output);
+    let packages = String::from_utf8(output.stdout).unwrap().split_whitespace().next().unwrap().to_owned();
+
+    // Disk
+    let output = Command::new("/bin/bash")
+        .args(&["-c", "df -h /"])
+        .output()
+        .expect("Failed to execute pacman");
+    let disk = String::from_utf8(output.stdout).unwrap();
+    let disk: Vec<&str> = disk.split_whitespace().collect();
+    let diskused = disk.get(10).expect("Unable to get disk used.");
+    let disktotal = disk.get(9).expect("Unable to get disk total.");
 
     // Print
-    println!("{:>9}  {} {}", "up".cyan().bold(), uptime.to_string().bold(), "minutes".bold());
-    println!("{:>9}  {}", "sh".blue().bold(), shell.bold());
-    println!("{:>10} {:.1} / {:.1} GB", "ram".yellow().bold(), (memused / 1_000_000.00).to_string().bold(), (memtotal / 1_000_000.00).to_string().bold());
-    println!("{:>10} {}", "ker".green().bold(), kernel.bold());
+    println!("{:>8}   {} {} {} {} {}", "up".cyan().bold(), hours.to_string().bold(), "hours".bold(), "and".bold(), minutes.to_string().bold(), "minutes".bold());
+    println!("{:>8}   {}", "sh".blue().bold(), shell.bold());
+    println!("{:>9}  {} / {} MB", "ram".yellow().bold(), (memused / 1_000.00).round().to_string().bold(), (memtotal / 1000.00).round().to_string().bold());
+    println!("{:>10} {}", "pkgs".red().bold(), packages.bold());
+    println!("{:>9}  {}", "ker".green().bold(), kernel.bold());
+    println!("{:>10} {} / {}", "disk".purple().bold(), diskused.bold(), disktotal.bold());
 }
